6a3ccca5d45324b4fd046825ffb71e2780088837
Emeric GAICHET <egaichet@snapp.fr>
2015-11-10 00:19:26 +0100

    Push and pull

--- lib/poulloustack.rb
+++ lib/poulloustack.rb
@@ -4,6 +4,14 @@ class PoullouStack
     @stack = []
   end
 
+  def push(element)
+    @stack << element
+  end
+
+  def pull
+    @stack.shift
+  end
+
   def size
     @stack.size
   end
--- test/poulloustack_test.rb
+++ test/poulloustack_test.rb
@@ -9,5 +9,21 @@ describe PoullouStack do
     it 'is empty on instantiation' do
       @stack.size.must_equal 0
     end
+
+    it 'can push elements' do
+      @stack.push('pouet')
+
+      @stack.size.must_equal 1
+    end
+
+    it 'can pull elements' do
+      pushed_element = 'pouet'
+
+      @stack.push(pushed_element)
+      pulled_element = @stack.pull
+
+      @stack.size.must_equal 0
+      pulled_element.must_equal pushed_element
+    end
   end
 end
