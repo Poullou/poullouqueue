7e0673f5f67614011582b41a3203f8b3fdcbbe45
Emeric GAICHET <egaichet@snapp.fr>
2015-11-10 23:37:48 +0100

    Push and pull many elements

--- lib/poulloustack.rb
+++ lib/poulloustack.rb
@@ -4,15 +4,19 @@ class PoullouStack
     @stack = []
   end
 
-  def push(element)
-    @stack << element
+  def push(*elements)
+    @stack.push(*elements)
   end
 
-  def pull
-    @stack.shift
+  def pull(amount = nil)
+    amount.nil? ? @stack.shift : @stack.shift(amount)
   end
 
   def size
     @stack.size
+  end
+
+  def empty?
+    @stack.empty?
   end
 end
--- test/poulloustack_test.rb
+++ test/poulloustack_test.rb
@@ -7,23 +7,38 @@ describe PoullouStack do
 
   describe 'Stack properties' do
     it 'is empty on instantiation' do
+      @stack.empty?.must_equal true
       @stack.size.must_equal 0
     end
 
-    it 'can push elements' do
+    it 'can push element' do
       @stack.push('pouet')
 
       @stack.size.must_equal 1
     end
 
-    it 'can pull elements' do
-      pushed_element = 'pouet'
+    it 'can pull element' do
+      @stack.push('pouet')
 
-      @stack.push(pushed_element)
       pulled_element = @stack.pull
 
       @stack.size.must_equal 0
-      pulled_element.must_equal pushed_element
+      pulled_element.must_equal 'pouet'
+    end
+
+    it 'can push many elements' do
+      @stack.push('pouet', 'poulou')
+
+      @stack.size.must_equal 2
+    end
+
+    it 'can pull many elements' do
+      @stack.push('pouet', 'poulou')
+
+      pulled_elments = @stack.pull(2)
+
+      @stack.size.must_equal 0
+      pulled_elments.size.must_equal 2
     end
   end
 end
